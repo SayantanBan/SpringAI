3a6ff4aa909fe9a16662bb7814e3fb4b6921b7ad
Sayantan Banerjee <sayantanbanerjee@outlook.com>
2026-04-16 10:11:22 +0530

    Structured Output in Spring AI

--- src/main/java/com/sayantan/mcpserver/config/ChatClientConfig.java
+++ src/main/java/com/sayantan/mcpserver/config/ChatClientConfig.java
@@ -15,7 +15,7 @@ public class ChatClientConfig {
     public ChatClient openAiChatClient(OpenAiChatModel openAiChatModel) {
 //        return ChatClient.create(openAiChatModel);
         ChatOptions chatOptions = ChatOptions.builder().model("gpt-4.1-mini")
-                .maxTokens(100)
+                .maxTokens(1000)
                 .temperature(0.8)
                 .build();
 
@@ -23,15 +23,6 @@ public class ChatClientConfig {
         return chatClientBuilder
                 .defaultOptions(chatOptions)
                 .defaultAdvisors(new SimpleLoggerAdvisor())
-                .defaultSystem("""
-                 You are an internal IT helpdesk assistant. Your role is to assist\s
-                 employees with IT-related issues such as resetting passwords,\s
-                 unlocking accounts, and answering questions related to IT policies.
-                 If a user requests help with anything outside of these\s
-                 responsibilities, respond politely and inform them that you are\s
-                 only able to assist with IT support tasks within your defined scope.
-                \s""")
-                .defaultUser("How can I help you?")
                 .build();
     }
 
---
+++ src/main/java/com/sayantan/mcpserver/controller/StructuredOutputController.java
@@ -0,0 +1,76 @@
+package com.sayantan.mcpserver.controller;
+
+import com.sayantan.mcpserver.model.CountryCities;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.converter.BeanOutputConverter;
+import org.springframework.ai.converter.ListOutputConverter;
+import org.springframework.ai.converter.MapOutputConverter;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class StructuredOutputController {
+
+    private final ChatClient chatClient;
+
+    public StructuredOutputController(@Qualifier("openAiChatClient") ChatClient chatClient) {
+        this.chatClient = chatClient;
+    }
+
+    @GetMapping("/chat-bean")
+    public ResponseEntity<CountryCities> chatBean(@RequestParam("message") String message) {
+        CountryCities countryCities = chatClient
+                .prompt()
+                .user(message)
+                .call()
+//                .entity(CountryCities.class)
+                .entity(new BeanOutputConverter<>(CountryCities.class));
+
+        return ResponseEntity.ok(countryCities);
+    }
+
+    @GetMapping("/chat-list")
+    public ResponseEntity<List<String>> chatList(@RequestParam("message") String message) {
+        List<String> cities = chatClient
+                .prompt()
+                .user(message)
+                .call()
+                .entity(new ListOutputConverter());
+
+        return ResponseEntity.ok(cities);
+    }
+
+    @GetMapping("/chat-map")
+    public ResponseEntity<Map<String, Object>> chatMap(@RequestParam("message") String message) {
+        Map<String, Object> cities = chatClient
+                .prompt()
+                .user(message)
+                .call()
+                .entity(new MapOutputConverter());
+
+        return ResponseEntity.ok(cities);
+    }
+
+    @GetMapping("/chat-bean-list")
+    public ResponseEntity<List<CountryCities>> chatBeanList(@RequestParam("message") String message) {
+        List<CountryCities> cities = chatClient
+                .prompt()
+                .user(message)
+                .call()
+                .entity(new ParameterizedTypeReference<List<CountryCities>>() {
+                });
+
+        return ResponseEntity.ok(cities);
+    }
+}
